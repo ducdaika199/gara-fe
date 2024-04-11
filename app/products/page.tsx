534dc5dd3266841446796c7da349bbb4f3dabe40
Ngoc-Nc <Fanvo@Fanvo.local>
2024-04-11 14:52:10 +0700

    add users page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -61,7 +61,7 @@ import {
 } from 'lucide-react';
 import React from 'react';
 
-const Product = () => {
+const Products = () => {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
@@ -547,4 +547,4 @@ const Product = () => {
   );
 };
 
-export default Product;
+export default Products;
