696958fa01431257fff02b950673c798e0a39f1e
ngocNC <ngoc-nc.nguyen@amplifyhealth.com>
2024-05-20 15:51:43 +0700

    add create product

--- src/components/products/productCreateForm.tsx
+++ src/components/products/productCreateForm.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import React from 'react';
-import { z } from 'zod';
 import {
   Form,
   FormControl,
@@ -10,15 +8,23 @@ import {
   FormLabel,
   FormMessage,
 } from '@/src/components/ui/form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 import { Input } from '@/src/components/ui/input';
-
-import { Button } from '../ui/button';
-import { createUser } from '@/src/lib/actions';
-import { useState, useEffect } from 'react';
 import { useToast } from '@/src/components/ui/use-toast';
+import { createProduct } from '@/src/lib/actions';
 import { cn } from '@/src/lib/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
+import CurrencyInput from 'react-currency-input-field';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Button } from '../ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
 
 const ProductCreateSheet = () => {
   const { toast } = useToast();
@@ -28,11 +34,9 @@ const ProductCreateSheet = () => {
     description: z.string(),
     countUnit: z.string(),
     priceUnit: z.string(),
-    carName: z.string(),
-    carType: z.string(),
     type: z.string(),
-    tax: z.number(),
-    ck: z.number(),
+    tax: z.string(),
+    ck: z.string(),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -43,28 +47,31 @@ const ProductCreateSheet = () => {
       description: '',
       countUnit: '',
       priceUnit: '',
-      carName: '',
-      carType: '',
-      type: '',
-      tax: 0,
-      ck: 0,
+      type: 'SUPPLIES',
+      tax: '0',
+      ck: '0',
     },
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-      const user = await createUser(values);
-      if (user) {
+      const product = await createProduct({
+        ...values,
+        priceUnit: Number(values.priceUnit),
+        tax: Number(values.tax),
+        ck: Number(values.ck),
+      });
+      if (product) {
         toast({
           className: cn(
             'top-0 left-2 flex fixed md:max-w-[420px] md:top-4 md:right-4]'
           ),
-          title: 'Tạo mới khách hàng thành công',
+          title: 'Tạo mới sản phẩm thành công',
           description: 'Thành công',
         });
         form.reset();
       }
-      return user;
+      return product;
     } catch (err) {
       toast({
         className: cn(
@@ -77,21 +84,17 @@ const ProductCreateSheet = () => {
   }
 
   return (
-    <div className='py-4'>
-      {/* <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+    <div className="py-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <FormField
             control={form.control}
-            name='username'
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Tên đăng nhập</FormLabel>
+                <FormLabel>Tên sản phẩm</FormLabel>
                 <FormControl>
-                  <Input
-                    id='username'
-                    placeholder='Tên đăng nhập...'
-                    {...field}
-                  />
+                  <Input id="name" placeholder="Tên sản phẩm..." {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -99,108 +102,155 @@ const ProductCreateSheet = () => {
           />
           <FormField
             control={form.control}
-            name='email'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Địa chỉ email</FormLabel>
-                <FormControl>
-                  <Input id='email' placeholder='Địa chỉ email...' {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='name'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tên</FormLabel>
-                <FormControl>
-                  <Input id='name' placeholder='Tên...' {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='phoneNumber'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Số điện thoại</FormLabel>
-                <FormControl>
-                  <Input
-                    id='phoneNumber'
-                    placeholder='Số điện thoại...'
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='plateNumber'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Biển số xe</FormLabel>
-                <FormControl>
-                  <Input
-                    id='plateNumber'
-                    placeholder='Biển số xe...'
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='carName'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tên xe</FormLabel>
-                <FormControl>
-                  <Input id='carName' placeholder='Tên xe...' {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='carType'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Loại xe</FormLabel>
-                <FormControl>
-                  <Input id='carType' placeholder='Loại xe...' {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='code'
+            name="code"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Mã</FormLabel>
                 <FormControl>
-                  <Input id='code' placeholder='Mã...' {...field} />
+                  <Input id="code" placeholder="Mã..." {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <div className='flex justify-end'>
-            <Button type='submit'>Lưu</Button>
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mô tả</FormLabel>
+                <FormControl>
+                  <Input id="description" placeholder="Mô tả..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="countUnit"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>ĐVT</FormLabel>
+                <FormControl>
+                  <Input
+                    id="countUnit"
+                    placeholder="Đơn vị tính..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="priceUnit"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Đơn giá</FormLabel>
+                  <FormControl>
+                    <CurrencyInput
+                      id="priceUnit"
+                      placeholder="đ1,234,567"
+                      allowDecimals={false}
+                      className={
+                        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
+                      }
+                      onValueChange={field.onChange}
+                      prefix={'đ'}
+                      step={10}
+                      name="priceUnit"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Loại</FormLabel>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Chọn vật công hoặc vật liệu" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="SUPPLIES">Vật liệu</SelectItem>
+                        <SelectItem value="REPAIRS">Vật công</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="tax"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Thuế</FormLabel>
+                  <FormControl>
+                    <CurrencyInput
+                      id="tax"
+                      placeholder="0%"
+                      allowDecimals={false}
+                      className={
+                        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
+                      }
+                      onValueChange={field.onChange}
+                      suffix={'%'}
+                      step={10}
+                      name="tax"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="ck"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Chiết khấu</FormLabel>
+                  <FormControl>
+                    <CurrencyInput
+                      id="ck"
+                      placeholder="0%"
+                      allowDecimals={false}
+                      className={
+                        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
+                      }
+                      onValueChange={field.onChange}
+                      suffix={'%'}
+                      step={10}
+                      name="ck"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <div className="flex justify-end">
+            <Button type="submit">Lưu</Button>
           </div>
         </form>
-      </Form> */}
+      </Form>
     </div>
   );
 };
