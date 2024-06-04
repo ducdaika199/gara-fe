ba3837fae295de63fe1362586eb8b1d5005c3ddb
ngocNC <ngoc-nc.nguyen@amplifyhealth.com>
2024-06-04 18:27:12 +0700

    create invoice

--- src/components/orders/orderCreateForm.tsx
+++ src/components/orders/orderCreateForm.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Check, ChevronsUpDown, XIcon } from 'lucide-react';
+import {
+  Check,
+  ChevronsUpDown,
+  MinusIcon,
+  PlusIcon,
+  XIcon,
+} from 'lucide-react';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -30,12 +36,13 @@ import {
   PopoverTrigger,
 } from '@/src/components/ui/popover';
 import { toast } from '@/src/components/ui/use-toast';
-import { getUsers } from '@/src/lib/actions';
+import { getProducts, getUsers } from '@/src/lib/actions';
 import { useDebouncedCallback } from 'use-debounce';
 import { useEffect, useState } from 'react';
-import { User } from '@prisma/client';
+import { Product, User } from '@prisma/client';
 import { log } from 'console';
 import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
 
 const FormSchema = z.object({
   userId: z.string({
@@ -57,6 +64,7 @@ const FormSchema = z.object({
 
 export default function OrderCreateForm() {
   const [users, setUsers] = useState<Partial<User[]>>([]);
+  const [products, setProducts] = useState<Partial<Product[]>>([]);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -66,7 +74,7 @@ export default function OrderCreateForm() {
     },
   });
 
-  const { fields, append } = useFieldArray({
+  const { fields, append, update } = useFieldArray({
     name: 'invoiceItems',
     control: form.control,
   });
@@ -75,140 +83,106 @@ export default function OrderCreateForm() {
     console.log(data, '-----submit data-------');
   }
 
-  console.log(
-    form.watch('invoiceItems'),
-    '------watch value invoice items------'
-  );
-
   const searchUsers = useDebouncedCallback(async (query: string) => {
     setUsers([]);
     const data = await getUsers(1, query);
     setUsers(data?.data ?? []);
   }, 300);
 
+  const searchProducts = useDebouncedCallback(async (query: string) => {
+    setProducts([]);
+    const data = await getProducts(1, query);
+    setProducts(data?.data ?? []);
+  }, 300);
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="userId"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Khách hàng :</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        'w-[200px] justify-between',
-                        !field.value && 'text-muted-foreground'
-                      )}
-                    >
-                      {field.value
-                        ? users.find(
-                            (user) => user?.id.toString() === field.value
-                          )?.name
-                        : 'Chọn khách hàng'}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput
-                      placeholder="Tìm kiếm khách hàng..."
-                      onValueChange={(value) => searchUsers(value)}
-                    />
-                    <CommandEmpty>Không tìm thấy khách hàng</CommandEmpty>
-                    <CommandGroup>
-                      <CommandList>
-                        {users.map((user) => (
-                          <CommandItem
-                            value={user?.name ?? ''}
-                            key={user?.id ?? ''}
-                            onSelect={() => {
-                              form.setValue(
-                                'userId',
-                                user?.id.toString() ?? ''
-                              );
-                            }}
-                          >
-                            <Check
-                              className={cn(
-                                'mr-2 h-4 w-4',
-                                user?.id.toString() === field.value
-                                  ? 'opacity-100'
-                                  : 'opacity-0'
-                              )}
-                            />
-                            {user?.name}
-                          </CommandItem>
-                        ))}
-                      </CommandList>
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="userRequest"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Y/c khách hàng</FormLabel>
-              <FormControl>
-                <Input
-                  id="username"
-                  placeholder="Y/c khách hàng ..."
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {fields.map((field, index) => {
-          return (
-            <>
-              <FormField
-                control={form.control}
-                name="invoiceItems"
-                key={index}
-                render={({ field }) => (
-                  <>
-                    <FormItem>
-                      <FormLabel>Form 1</FormLabel>
-                      <FormControl>
-                        <Input
-                          id="productId"
-                          placeholder="Y/c khách hàng ..."
-                          {...form.register(`invoiceItems.${index}.productId`)}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                    <FormItem>
-                      <FormLabel>Form 2</FormLabel>
-                      <FormControl>
-                        <Input
-                          id="quantity"
-                          placeholder="Y/c khách hàng ..."
-                          {...form.register(`invoiceItems.${index}.quantity`)}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  </>
-                )}
-              />
-            </>
-          );
-        })}
+        <div className="grid grid-flow-col gap-2">
+          <FormField
+            control={form.control}
+            name="userId"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Khách hàng: </FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          'w-[200px] justify-between mt-8',
+                          !field.value && 'text-muted-foreground'
+                        )}
+                      >
+                        {field.value
+                          ? users.find(
+                              (user) => user?.id.toString() === field.value
+                            )?.name
+                          : 'Chọn khách hàng'}
+                        <ChevronsUpDown className="ml-2  h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    <Command>
+                      <CommandInput
+                        placeholder="Tìm kiếm khách hàng..."
+                        onValueChange={(value) => searchUsers(value)}
+                      />
+                      <CommandEmpty>Không tìm thấy khách hàng</CommandEmpty>
+                      <CommandGroup>
+                        <CommandList>
+                          {users.map((user) => (
+                            <CommandItem
+                              value={user?.name ?? ''}
+                              key={user?.id ?? ''}
+                              onSelect={() => {
+                                form.setValue(
+                                  'userId',
+                                  user?.id.toString() ?? ''
+                                );
+                              }}
+                            >
+                              <Check
+                                className={cn(
+                                  'mr-2 h-4 w-4',
+                                  user?.id.toString() === field.value
+                                    ? 'opacity-100'
+                                    : 'opacity-0'
+                                )}
+                              />
+                              {user?.name}
+                            </CommandItem>
+                          ))}
+                        </CommandList>
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="userRequest"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Yêu cầu của khách hàng: </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Điền thông tin yêu cầu của khách hàng ..."
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <Button
           type="button"
           onClick={() =>
@@ -218,8 +192,156 @@ export default function OrderCreateForm() {
             })
           }
         >
-          APPEND
+          + Thêm mới
         </Button>
+        {fields.map((field, index) => {
+          return (
+            <div
+              key={field.id}
+              className="grid grid-flow-col gap-2 items-center"
+            >
+              <FormField
+                control={form.control}
+                name="invoiceItems"
+                key={index}
+                render={() => {
+                  return (
+                    <>
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Sản phẩm: </FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant="outline"
+                                role="combobox"
+                                className={cn(
+                                  'w-[200px] justify-between mt-8',
+                                  !field.id && 'text-muted-foreground'
+                                )}
+                              >
+                                {field.productId
+                                  ? field.productId
+                                  : 'Chọn sản phẩm'}
+
+                                <ChevronsUpDown className="ml-2  h-4 w-4 shrink-0 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-[200px] p-0">
+                            <Command>
+                              <CommandInput
+                                id="productId"
+                                placeholder="Tìm kiếm sản phẩm..."
+                                {...form.register(
+                                  `invoiceItems.${index}.productId`
+                                )}
+                                onValueChange={(value) => searchProducts(value)}
+                              />
+                              <CommandEmpty>
+                                Không tìm thấy sản phẩm
+                              </CommandEmpty>
+                              <CommandGroup>
+                                <CommandList>
+                                  {products.map((product) => {
+                                    return (
+                                      <CommandItem
+                                        value={product?.name ?? ''}
+                                        key={product?.id ?? ''}
+                                        onSelect={() => {
+                                          update(index, {
+                                            productId:
+                                              product?.id.toString() ?? '',
+                                            quantity: field.quantity ?? '1',
+                                          });
+                                        }}
+                                      >
+                                        <Check
+                                          className={cn(
+                                            'mr-2 h-4 w-4',
+                                            product?.id.toString() ===
+                                              field.productId
+                                              ? 'opacity-100'
+                                              : 'opacity-0'
+                                          )}
+                                        />
+                                        {product?.name}
+                                      </CommandItem>
+                                    );
+                                  })}
+                                </CommandList>
+                              </CommandGroup>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                      <FormItem>
+                        <FormLabel>Số lượng: </FormLabel>
+                        <FormControl>
+                          <div className="flex items-center gap-2">
+                            <Button
+                              variant="ghost"
+                              size="icon"
+                              onClick={() => {
+                                if (Number(field.quantity) > 1) {
+                                  update(index, {
+                                    productId: field.productId,
+                                    quantity: (
+                                      Number(field.quantity) - 1
+                                    ).toString(),
+                                  });
+                                }
+                              }}
+                              className="h-10 w-10 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+                            >
+                              <MinusIcon className="h-5 w-5" />
+                              <span className="sr-only">Decrement</span>
+                            </Button>
+                            <Input
+                              id="quantity"
+                              type="number"
+                              value={field.quantity}
+                              {...form.register(
+                                `invoiceItems.${index}.quantity`
+                              )}
+                              onChange={(e) =>
+                                update(index, {
+                                  productId: field.productId,
+                                  quantity: e.target.value,
+                                })
+                              }
+                              className="w-max text-center"
+                              min={1}
+                            />
+                            <Button
+                              variant="ghost"
+                              size="icon"
+                              onClick={() =>
+                                update(index, {
+                                  productId: field.productId,
+                                  quantity: (
+                                    Number(field.quantity) + 1
+                                  ).toString(),
+                                })
+                              }
+                              className="h-10 w-10 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800"
+                            >
+                              <PlusIcon className="h-5 w-5" />
+                              <span className="sr-only">Increment</span>
+                            </Button>
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    </>
+                  );
+                }}
+              />
+            </div>
+          );
+        })}
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
